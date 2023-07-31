530fc5bca677e88de53268fd50c4933981c8e33d
kerrat <v.vlad.demid@gmail.com>
2023-07-31 19:16:47 +0300

    update posting method

--- httpPostUntraslatedFile.cs
+++ httpPostUntraslatedFile.cs
@@ -12,6 +12,8 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using dbAdministration;
 using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace dbAdministration
 {
@@ -30,43 +32,63 @@ namespace dbAdministration
         {
             string filename = req.Query["filename"];
 
-            if (string.IsNullOrWhiteSpace(filename) || req == null || string.IsNullOrWhiteSpace(req.Body.ToString()))
+            if (string.IsNullOrWhiteSpace(filename) || req.Body == null)
             {
                 return new BadRequestResult();
             }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var data = JsonSerializer.Deserialize<Dictionary<long, string>>(requestBody);
+            var data = JsonSerializer.Deserialize<Dictionary<long, string>>(requestBody, new JsonSerializerOptions
+                {
+                    NumberHandling = JsonNumberHandling.AllowReadingFromString
+                });
 
-            foreach (var item in data)
+            if (data == null || data.Count == 0)
             {
-                var existingTranslation = await _context.Translations.FirstOrDefaultAsync(p => p.Id == item.Key);
-
-                if (existingTranslation != null)
-                {
-                    if (existingTranslation.UntranslatedText != item.Value)
-                    {
-                        existingTranslation.UntranslatedText = item.Value;
-                        existingTranslation.TranslateStatus = "Untranslated";
-                        existingTranslation.ChangedDate = DateTime.UtcNow;
-                    }
-                }
-                else
-                {
-                    var translation = new Translation
-                    {
-                        Id = item.Key,
-                        UntranslatedText = item.Value,
-                        TranslateStatus = "Untranslated",
-                        FileName = filename,
-                        ChangedDate = DateTime.UtcNow
-                    };
-
-                    await _context.Translations.AddAsync(translation);
-                }
+                return new BadRequestResult();
             }
 
-            await _context.SaveChangesAsync();
+            var idsToSearch = data.Keys;
+
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                var allIdsInDatabase = await _context.Translations.Select(p => p.Id).ToListAsync();
+                var idsToDelete = allIdsInDatabase.Except(idsToSearch);
+                _context.Translations.RemoveRange(_context.Translations.Where(p => idsToDelete.Contains(p.Id)));
+
+                var existingTranslations = await _context.Translations.Where(p => idsToSearch.Contains(p.Id)).ToListAsync();
+
+                foreach (var item in data)
+                {
+                    var existingTranslation = existingTranslations.FirstOrDefault(p => p.Id == item.Key);
+
+                    if (existingTranslation != null)
+                    {
+                        if (existingTranslation.UntranslatedText != item.Value)
+                        {
+                            existingTranslation.UntranslatedText = item.Value;
+                            existingTranslation.TranslateStatus = "Untranslated";
+                            existingTranslation.ChangedDate = DateTime.UtcNow;
+                        }
+                    }
+                    else
+                    {
+                        var translation = new Translation
+                        {
+                            Id = item.Key,
+                            UntranslatedText = item.Value,
+                            TranslateStatus = "Untranslated",
+                            FileName = filename,
+                            ChangedDate = DateTime.UtcNow
+                        };
+
+                        await _context.Translations.AddAsync(translation);
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
 
             return new OkResult();
         }
